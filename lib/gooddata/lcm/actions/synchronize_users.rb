a74472548ce885a00b17cb82e2f053eaaac922b6
hung.nguyen <hung.nguyen@gooddata.com>
2019-10-04 13:26:34 +0700

    BUGFIX: TMA-1580 Process read large CSV file

--- lib/gooddata/lcm/actions/synchronize_users.rb
+++ lib/gooddata/lcm/actions/synchronize_users.rb
@@ -366,14 +366,14 @@ module GoodData
           sso_provider = params.sso_provider
           authentication_modes = params.authentication_modes || []
 
-          tmp = without_check(PARAMS, params) do
-            File.open(data_source.realize(params), 'r:UTF-8')
-          end
-
-          begin
-            data = read_csv_file(tmp)
-          rescue Exception => e # rubocop:disable RescueException
-            fail "There was an error during loading users from csv file. Message: #{e.message}. Error: #{e}"
+          dwh = params.ads_client
+          if dwh
+            data = dwh.execute_select(params.input_source.query)
+          else
+            tmp = without_check(PARAMS, params) do
+              File.open(data_source.realize(params), 'r:UTF-8')
+            end
+            data = read_csv_file(tmp, params)
           end
 
           data.map do |row|
@@ -420,27 +420,25 @@ module GoodData
           end
         end
 
-        def read_csv_file(path)
-          GoodData.logger.info('Start reading csv file')
+        def read_csv_file(path, params)
+          params.gdc_logger.debug('Start reading csv file')
           res = []
           row_count = 0
 
-          CSV.foreach(path, :headers => true, :header_converters => :downcase, :encoding => 'utf-8') do |row|
+          CSV.foreach(path, :headers => true) do |row|
+            row_count += 1
+
             if block_given?
               data = yield row
             else
               data = row
             end
 
-            if data
-              row_count += 1
-              res << data
-            end
-
-            GoodData.logger.info("Read #{row_count} rows") if (row_count % 50_000).zero?
+            res << data if data
+            params.gdc_logger.debug("Read #{row_count} rows") if (row_count % 50_000).zero?
           end
 
-          GoodData.logger.info("Done reading csv file, total #{row_count} rows")
+          params.gdc_logger.debug("Done reading csv file, total #{row_count} rows")
           res
         end
       end
